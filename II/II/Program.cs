2523335b57851adf44f105b91420021413d67338
[1937forza] <[kadirofff5@outlook.com]>
2021-10-25 12:44:29 +0400

    Changed

---
+++ II/II/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. 1-den 8000-dek ededler icerisinde cut olanlarin sayini tapan proqram
+            //int sum = 0;
+            //for(int i=2; i < 8000; i = i + 2)
+            //{
+            //    sum++;
+            //    Console.WriteLine(sum);
+            //}
+
+            //1. Verilmis array-deki reqemlerinin cemi 10-dan kicik olan ededlerin sayini tapan alqoritm
+            //int digitcounts = 0;
+            //var num = 0;
+            //int[] numbers = new int[10] { 2, 12, 45, 36, 78, 123, 455, 566, 447, 888 };
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    num = numbers[i];
+
+
+            //    while (num > 0)
+            //    {
+            //        var lastDigit = num % 10;
+            //        digitcounts += lastDigit;
+            //        num = (num - lastDigit) / 10;
+            //    }
+
+            //    if (10 > digitcounts)
+            //    {
+            //        Console.WriteLine(digitcounts);
+            //    }
+
+
+            //}
+
+            //3. Verilmis ededin en yaxin asagi kokalti deyerini tapan alqoritm (kecen ders etdiyimize uygun, misalcun eded 20-dirse 4 qaytarmalidi, 25-dise 5 qaytarmalidi, 16-drisa 4 ve s.)
+
+            //int N = 56;
+            //var i = 1;
+            //var result =0;
+
+            //while (i * i <= N)
+            //{
+            //    i++;
+            //}
+            //result = i - 1;
+
+
+            //Console.WriteLine(result);
+
+            }
+    }
+}
